3efc7de369d038c637e7db4d1f3b5f8ed3f57e8b
uha0001 <uha0001@auburn.edu>
2020-03-11 14:57:45 -0500

    last part longest peptide solved

--- translate.py
+++ translate.py
@@ -182,7 +182,23 @@ def get_longest_peptide(rna_sequence, genetic_code):
         A string of the longest sequence of amino acids encoded by
         `rna_sequence`.
     """
-    pass
+    protein = get_all_translations(rna_sequence = rna_sequence,
+            genetic_code = genetic_code)
+    rev_comp_seq =  reverse_and_complement(rna_sequence)
+    rev_comp_peptides = get_all_translations(rna_sequence = rev_comp_seq,
+            genetic_code = genetic_code)
+    protein += rev_comp_peptides
+    if not protein:
+        return ""
+    if len(protein) < 2:
+        return protein[0]
+    most_number_of_bases = -1
+    longest_peptide_index = -1
+    for peptide_index, aa_seq in enumerate(protein):
+        if len(aa_seq) > most_number_of_bases:
+            longest_peptide_index = peptide_index
+            most_number_of_bases = len(aa_seq)
+    return protein[longest_peptide_index]
 
 
 if __name__ == '__main__':
