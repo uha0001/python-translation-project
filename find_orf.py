e0ff4456c14ed3e676321898022c1e3f94e7c37b
uha0001 <uha0001@auburn.edu>
2020-04-05 17:55:21 -0500

    all test resolved

--- find_orf.py
+++ find_orf.py
@@ -57,8 +57,8 @@ def vet_nucleotide_sequence(sequence):
     # any valid RNA and DNA sequence strings, respectively (and only strings of
     # RNA and DNA bases).
     # Read the docstring above for additional clues.
-    rna_pattern_str = r'AUCG'
-    dna_pattern_str = r'ATCG'
+    rna_pattern_str = r'^[AUCGaucg]*$'
+    dna_pattern_str = r'^[ATCGatcg]*$'
     ##########################################################################
 
     rna_pattern = re.compile(rna_pattern_str)
@@ -119,7 +119,7 @@ def vet_codon(codon):
     # Change `codon_pattern_str` so that it will match any valid codons, and
     # only valid codons.
     # Read the docstring above for additional clues.
-    codon_pattern_str = r'AUG'
+    codon_pattern_str = r'^[A|U|G|C|a|u|g|c]{3}$'
     ##########################################################################
 
     codon_pattern = re.compile(codon_pattern_str)
@@ -207,8 +207,9 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
-    ##########################################################################
+    #hardcoding_orf_pattern_str = r'AUG'+r'([A|U|C|G]{3})*('+r'UAA|UGA|UAG'+r')'
+    orf_pattern_str = r'('+r'|'.join(starts)+r')([AUCG]{3})*('+r'|'.join(stops)+r')'
+##########################################################################
 
     # Create the regular expression object
     orf_pattern = re.compile(orf_pattern_str)
